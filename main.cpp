270281edbc74c880e0c056c59faa73ea71fa1f22
hpc203 <1749069040@qq.com>
2021-02-18 10:16:16 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,239 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <fstream>
+#include <iostream>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+using namespace dnn;
+using namespace std;
+
+class NanoDet
+{
+	public:
+		NanoDet(float confThreshold, float nmsThreshold);
+		void detect(Mat& srcimg);
+
+	private:
+		const int stride[3] = { 8, 16, 32 };
+		const string classesFile = "coco.names";
+		const int input_shape[2] = { 320,320 };   //// height, width
+		const float mean[3] = { 103.53, 116.28, 123.675 };
+		const float std[3] = { 57.375, 57.12, 58.395 };
+		const int reg_max = 7;
+		float prob_threshold;
+		float iou_threshold;
+        vector<string> classes;
+        int num_class;
+		Net net;
+
+        Mat resize_image(Mat srcimg, int* newh, int* neww, int* top, int* left);
+        void normalize(Mat& srcimg);
+        void softmax(float* x, int length);
+        void post_process(vector<Mat> outs, Mat& frame, int newh, int neww, int top, int left);
+        void generate_proposal(vector<int>& classIds, vector<float>& confidences, vector<Rect>& boxes, const int stride_, Mat out_score, Mat out_box);
+		const bool keep_ratio = true;
+};
+
+NanoDet::NanoDet(float confThreshold, float nmsThreshold)
+{
+	this->prob_threshold = confThreshold;
+	this->iou_threshold = nmsThreshold;
+
+	ifstream ifs(this->classesFile.c_str());
+	string line;
+	while (getline(ifs, line)) this->classes.push_back(line);
+    this->num_class = this->classes.size();
+	this->net = readNet("nanodet.onnx");
+}
+
+Mat NanoDet::resize_image(Mat srcimg, int* newh, int* neww, int* top, int* left)
+{
+	int srch = srcimg.rows, srcw = srcimg.cols;
+	*newh = this->input_shape[0];
+	*neww = this->input_shape[1];
+	Mat dstimg;
+	if (this->keep_ratio && srch != srcw)
+	{
+		float hw_scale = (float)srch / srcw;
+		if (hw_scale > 1)
+		{
+			*newh = this->input_shape[0];
+			*neww = int(this->input_shape[1] / hw_scale);
+			resize(srcimg, dstimg, Size(*neww, *newh), INTER_AREA);
+			*left = int((this->input_shape[1] - *neww) * 0.5);
+			copyMakeBorder(dstimg, dstimg, 0, 0, *left, this->input_shape[1] - *neww - *left, BORDER_CONSTANT, 0);
+		}
+		else
+		{
+			*newh = (int)this->input_shape[0] * hw_scale;
+			*neww = this->input_shape[1];
+			resize(srcimg, dstimg, Size(*neww, *newh), INTER_AREA);
+			*top = (int)(this->input_shape[0] - *newh) * 0.5;
+			copyMakeBorder(dstimg, dstimg, *top, this->input_shape[0] - *newh - *top, 0, 0, BORDER_CONSTANT, 0);
+		}
+	}
+	else
+	{
+		resize(srcimg, dstimg, Size(*neww, *newh), INTER_AREA);
+	}
+	return dstimg;
+}
+
+void NanoDet::normalize(Mat& img)
+{
+    img.convertTo(img, CV_32F);
+	int i = 0, j = 0;
+	for (i = 0; i < img.rows; i++)
+	{
+		float* pdata = (float*)(img.data + i * img.step);
+		for (j = 0; j < img.cols; j++)
+		{
+			pdata[0] = (pdata[0] - this->mean[0]) / this->std[0];
+			pdata[1] = (pdata[1] - this->mean[1]) / this->std[1];
+			pdata[2] = (pdata[2] - this->mean[2]) / this->std[2];
+			pdata += 3;
+		}
+	}
+}
+
+//Mat NanoDet::normalize(Mat src)
+//{
+//	vector<Mat> bgrChannels(3);
+//	split(src, bgrChannels);
+//	for (auto i = 0; i < bgrChannels.size(); i++)
+//	{
+//		bgrChannels[i].convertTo(bgrChannels[i], CV_32FC1, 1.0 / this->std[i], (0.0 - this->mean[i]) / this->std[i]);
+//	}
+//	Mat dst;
+//	merge(bgrChannels, dst);
+//	return dst;
+//}
+
+void NanoDet::detect(Mat& srcimg)
+{
+	int newh = 0, neww = 0, top = 0, left = 0;
+	Mat dstimg = this->resize_image(srcimg, &newh, &neww, &top, &left);
+	this->normalize(dstimg);
+    Mat blob = blobFromImage(dstimg);
+
+	this->net.setInput(blob);
+	vector<Mat> outs;
+	this->net.forward(outs, this->net.getUnconnectedOutLayersNames());
+	this->post_process(outs, srcimg, newh, neww, top, left);
+}
+
+void NanoDet::softmax(float* x, int length)
+{
+	float sum = 0;
+	int i = 0;
+	for (i = 0; i < length; i++)
+	{
+		x[i] = exp(x[i]);
+		sum += x[i];
+	}
+	for (i = 0; i < length; i++)
+	{
+		x[i] /= sum;
+	}
+}
+
+void NanoDet::generate_proposal(vector<int>& classIds, vector<float>& confidences, vector<Rect>& boxes, const int stride_, Mat out_score, Mat out_box)
+{
+    const int num_grid_y = (int)this->input_shape[0]/stride_;
+    const int num_grid_x = (int)this->input_shape[1]/stride_;
+    const int reg_1max = this->reg_max + 1;
+    for (int i = 0; i < num_grid_y; i++)
+    {
+        for (int j = 0; j < num_grid_x; j++)
+        {
+            const int idx = i * num_grid_x + j;
+			Mat scores = out_score.row(idx).colRange(0, num_class);
+			Point classIdPoint;
+			double score;
+			// Get the value and location of the maximum score
+			minMaxLoc(scores, 0, &score, 0, &classIdPoint);
+            if (score >= this->prob_threshold)
+            {
+                float* pbox = (float*)out_box.data + idx * reg_1max * 4;
+                float dis_pred[4];
+                for (int k = 0; k < 4; k++)
+                {
+                    this->softmax(pbox, reg_1max);
+                    float dis = 0.f;
+                    for (int l = 0; l < reg_1max; l++)
+                    {
+                        dis += l * pbox[l];
+                    }
+                    dis_pred[k] = dis * stride_;
+                    pbox += reg_1max;
+                }
+
+				float pb_cx = (j + 0.5f) * stride_ - 0.5;
+                float pb_cy = (i + 0.5f) * stride_ - 0.5;       
+                float x0 = pb_cx - dis_pred[0];
+                float y0 = pb_cy - dis_pred[1];
+                float x1 = pb_cx + dis_pred[2];
+                float y1 = pb_cy + dis_pred[3];
+
+                classIds.push_back(classIdPoint.x);
+                confidences.push_back(score);
+                boxes.push_back(Rect((int)x0, (int)y0, (int)(x1 - x0), (int)(y1 - y0)));
+            }
+        }
+    }
+}
+
+void NanoDet::post_process(vector<Mat> outs, Mat& frame, int newh, int neww, int top, int left)
+{
+    /////generate proposals
+    vector<int> classIds;
+    vector<float> confidences;
+    vector<Rect> boxes;
+    this->generate_proposal(classIds, confidences, boxes, this->stride[0], outs[0], outs[1]);
+    this->generate_proposal(classIds, confidences, boxes, this->stride[1], outs[2], outs[3]);
+    this->generate_proposal(classIds, confidences, boxes, this->stride[2], outs[4], outs[5]);
+
+    // Perform non maximum suppression to eliminate redundant overlapping boxes with
+    // lower confidences
+    vector<int> indices;
+    NMSBoxes(boxes, confidences, this->prob_threshold, this->iou_threshold, indices);
+    float ratioh = (float)frame.rows / newh;
+    float ratiow = (float)frame.cols / neww;
+    for (size_t i = 0; i < indices.size(); ++i)
+    {
+        int idx = indices[i];
+        Rect box = boxes[idx];
+        int xmin = (int)max((box.x - left)*ratiow, 0.f);
+        int ymin = (int)max((box.y - top)*ratioh, 0.f);
+        int xmax = (int)min((box.x - left + box.width)*ratiow, (float)frame.cols);
+        int ymax = (int)min((box.y - top + box.height)*ratioh, (float)frame.rows);
+        rectangle(frame, Point(xmin, ymin), Point(xmax, ymax), Scalar(0, 0, 255), 3);
+        //Get the label for the class name and its confidence
+        string label = format("%.2f", confidences[idx]);
+        label = classes[classIds[idx]] + ":" + label;
+
+        //Display the label at the top of the bounding box
+        int baseLine;
+        Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+        ymin = max(ymin, labelSize.height);
+        //rectangle(frame, Point(left, top - int(1.5 * labelSize.height)), Point(left + int(1.5 * labelSize.width), top + baseLine), Scalar(0, 255, 0), FILLED);
+        putText(frame, label, Point(xmin, ymin), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 255, 0), 1);
+    }
+}
+
+int main()
+{
+	NanoDet nanonet(0.35, 0.6);
+
+	string imgpath = "street.png";
+	Mat srcimg = imread(imgpath);
+	nanonet.detect(srcimg);
+
+	static const string kWinName = "Deep learning object detection in OpenCV";
+	namedWindow(kWinName, WINDOW_NORMAL);
+	imshow(kWinName, srcimg);
+	waitKey(0);
+	destroyAllWindows();
+}
---
+++ main_nanodet.py
@@ -0,0 +1,169 @@
+import cv2
+import numpy as np
+import argparse
+
+class my_nanodet():
+    def __init__(self, input_shape=[320, 320], reg_max=7, strides=[8, 16, 32], prob_threshold=0.4, iou_threshold=0.3):
+        with open('coco.names', 'rt') as f:
+            self.classes = f.read().rstrip('\n').split('\n')
+        self.num_classes = len(self.classes)
+        self.strides = strides
+        self.input_shape = tuple(input_shape)
+        self.reg_max = reg_max
+        self.prob_threshold = prob_threshold
+        self.iou_threshold = iou_threshold
+        self.project = np.arange(self.reg_max + 1)
+
+        self.mlvl_anchors = []
+        for i in range(len(self.strides)):
+            anchors = self._make_grid((int(input_shape[0] / strides[i]), int(input_shape[1] / strides[i])), strides[i])
+            self.mlvl_anchors.append(anchors)
+
+        self.mean = np.array([103.53, 116.28, 123.675], dtype=np.float32).reshape(1, 1, 3)
+        self.std = np.array([57.375, 57.12, 58.395], dtype=np.float32).reshape(1, 1, 3)
+        self.net = cv2.dnn.readNet('nanodet.onnx')
+    def _make_grid(self, featmap_size, stride):
+        feat_h, feat_w = featmap_size
+        shift_x = np.arange(0, feat_w) * stride
+        shift_y = np.arange(0, feat_h) * stride
+        xv, yv = np.meshgrid(shift_x, shift_y)
+        xv = xv.flatten()
+        yv = yv.flatten()
+        cx = xv + 0.5 * (stride-1)
+        cy = yv + 0.5 * (stride - 1)
+        return np.stack((cx, cy), axis=-1)
+    def softmax(self,x, axis=1):
+        x_exp = np.exp(x)
+        # 如果是列向量，则axis=0
+        x_sum = np.sum(x_exp, axis=axis, keepdims=True)
+        s = x_exp / x_sum
+        return s
+
+    def _normalize(self, img):   ### c++: https://blog.csdn.net/wuqingshan2010/article/details/107727909
+        img = img.astype(np.float32)
+        img = (img - self.mean) / self.std
+        return img
+    def resize_image(self, srcimg, keep_ratio=True):
+        top, left, newh, neww = 0, 0, self.input_shape[0], self.input_shape[1]
+        if keep_ratio and srcimg.shape[0] != srcimg.shape[1]:
+            hw_scale = srcimg.shape[0] / srcimg.shape[1]
+            if hw_scale > 1:
+                newh, neww = self.input_shape[0], int(self.input_shape[1] / hw_scale)
+                img = cv2.resize(srcimg, (neww, newh), interpolation=cv2.INTER_AREA)
+                left = int((self.input_shape[1] - neww) * 0.5)
+                img = cv2.copyMakeBorder(img, 0, 0, left, self.input_shape[1] - neww - left, cv2.BORDER_CONSTANT,
+                                         value=0)  # add border
+            else:
+                newh, neww = int(self.input_shape[0] * hw_scale), self.input_shape[1]
+                img = cv2.resize(srcimg, (neww, newh), interpolation=cv2.INTER_AREA)
+                top = int((self.input_shape[0] - newh) * 0.5)
+                img = cv2.copyMakeBorder(img, top, self.input_shape[0] - newh - top, 0, 0, cv2.BORDER_CONSTANT, value=0)
+        else:
+            img = cv2.resize(srcimg, self.input_shape, interpolation=cv2.INTER_AREA)
+        return img, newh, neww, top, left
+    def detect(self, srcimg):
+        img, newh, neww, top, left = self.resize_image(srcimg)
+        img = self._normalize(img)
+        blob = cv2.dnn.blobFromImage(img)
+        # Sets the input to the network
+        self.net.setInput(blob)
+
+        # Runs the forward pass to get output of the output layers
+        outs = self.net.forward(self.net.getUnconnectedOutLayersNames())
+        det_bboxes, det_conf, det_classid = self.post_process(outs)
+
+        drawimg = srcimg.copy()
+        ratioh,ratiow = srcimg.shape[0]/newh,srcimg.shape[1]/neww
+        for i in range(det_bboxes.shape[0]):
+            xmin, ymin, xmax, ymax = max(int((det_bboxes[i,0] - left) * ratiow), 0), max(int((det_bboxes[i,1] - top) * ratioh), 0), min(
+                int((det_bboxes[i,2] - left) * ratiow), srcimg.shape[1]), min(int((det_bboxes[i,3] - top) * ratioh), srcimg.shape[0])
+            self.drawPred(drawimg, det_classid[i], det_conf[i], xmin, ymin, xmax, ymax)
+        return drawimg
+
+    def post_process(self, preds):
+        cls_scores, bbox_preds = preds[::2], preds[1::2]
+        det_bboxes, det_conf, det_classid = self.get_bboxes_single(cls_scores, bbox_preds, 1, rescale=False)
+        return det_bboxes.astype(np.int32), det_conf, det_classid
+    def get_bboxes_single(self, cls_scores, bbox_preds, scale_factor, rescale=False):
+        mlvl_bboxes = []
+        mlvl_scores = []
+        for stride, cls_score, bbox_pred, anchors in zip(self.strides, cls_scores, bbox_preds, self.mlvl_anchors):
+            bbox_pred = self.softmax(bbox_pred.reshape(-1, self.reg_max + 1), axis=1)
+            # bbox_pred = np.sum(bbox_pred * np.expand_dims(self.project, axis=0), axis=1).reshape((-1, 4))
+            bbox_pred = np.dot(bbox_pred, self.project).reshape(-1,4)
+            bbox_pred *= stride
+
+            # nms_pre = cfg.get('nms_pre', -1)
+            nms_pre = 1000
+            if nms_pre > 0 and cls_score.shape[0] > nms_pre:
+                max_scores = cls_score.max(axis=1)
+                topk_inds = max_scores.argsort()[::-1][0:nms_pre]
+                anchors = anchors[topk_inds, :]
+                bbox_pred = bbox_pred[topk_inds, :]
+                cls_score = cls_score[topk_inds, :]
+
+            bboxes = self.distance2bbox(anchors, bbox_pred, max_shape=self.input_shape)
+            mlvl_bboxes.append(bboxes)
+            mlvl_scores.append(cls_score)
+
+        mlvl_bboxes = np.concatenate(mlvl_bboxes, axis=0)
+        if rescale:
+            mlvl_bboxes /= scale_factor
+        mlvl_scores = np.concatenate(mlvl_scores, axis=0)
+
+        bboxes_wh = mlvl_bboxes.copy()
+        bboxes_wh[:, 2:4] = bboxes_wh[:, 2:4] - bboxes_wh[:, 0:2]  ####xywh
+        classIds = np.argmax(mlvl_scores, axis=1)
+        confidences = np.max(mlvl_scores, axis=1)  ####max_class_confidence
+
+        indices = cv2.dnn.NMSBoxes(bboxes_wh.tolist(), confidences.tolist(), self.prob_threshold, self.iou_threshold)
+        mlvl_bboxes = mlvl_bboxes[indices[:, 0]]
+        confidences = confidences[indices[:, 0]]
+        classIds = classIds[indices[:, 0]]
+        return mlvl_bboxes, confidences, classIds
+    def distance2bbox(self, points, distance, max_shape=None):
+        x1 = points[:, 0] - distance[:, 0]
+        y1 = points[:, 1] - distance[:, 1]
+        x2 = points[:, 0] + distance[:, 2]
+        y2 = points[:, 1] + distance[:, 3]
+        if max_shape is not None:
+            x1 = np.clip(x1, 0, max_shape[1])
+            y1 = np.clip(y1, 0, max_shape[0])
+            x2 = np.clip(x2, 0, max_shape[1])
+            y2 = np.clip(y2, 0, max_shape[0])
+        return np.stack([x1, y1, x2, y2], axis=-1)
+
+    def drawPred(self, frame, classId, conf, left, top, right, bottom):
+        # Draw a bounding box.
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), thickness=4)
+
+        label = '%.2f' % conf
+        label = '%s:%s' % (self.classes[classId], label)
+
+        # Display the label at the top of the bounding box
+        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        top = max(top, labelSize[1])
+        # cv.rectangle(frame, (left, top - round(1.5 * labelSize[1])), (left + round(1.5 * labelSize[0]), top + baseLine), (255,255,255), cv.FILLED)
+        cv2.putText(frame, label, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), thickness=2)
+        return frame
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--imgpath', type=str, default='street.png', help="image path")
+    parser.add_argument('--confThreshold', default=0.35, type=float, help='class confidence')
+    parser.add_argument('--nmsThreshold', default=0.6, type=float, help='nms iou thresh')
+    args = parser.parse_args()
+
+    srcimg = cv2.imread(args.imgpath)
+    net = my_nanodet(prob_threshold=args.confThreshold, iou_threshold=args.nmsThreshold)
+    import time
+    a = time.time()
+    srcimg = net.detect(srcimg)
+    b = time.time()
+    print('waste time', b-a)
+
+    winName = 'Deep learning object detection in OpenCV'
+    cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
+    cv2.imshow(winName, srcimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
